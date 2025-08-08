2814d166d46b0e897514c1d31c26b131dde0ff92
jeet yaduvanshi <jeetxyadav@gmail.com>
2025-08-08 19:59:05 +0530

    something

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,6 +8,7 @@ const Banner = () => {
         <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Welcome to Our Blog</h1>
         <p className='text-gray-100 lg:w-3/5 mx-auto mb-8 font-primary'>Start your blog today and join a community of writers and readers who are passionate about sharing their stories and ideas. We offer everything you need to get started, from helpful tips and tutorials.</p>
         <div className='flex justify-center gap-4 flex-wrap'>
+          
           <Link
             to="/blogs"
             className='font-medium hover:text-orange-500 inline-flex items-center py-2 px-6 bg-gray-800 rounded-lg transition-all duration-300 hover:bg-gray-700'
